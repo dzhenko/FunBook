d178046e1e591f978544fe47d3a2c8ab114f8352
dpesheva <dkshosa@gmail.com>
2014-10-21 19:40:13 +0300

    fill categories menu

--- FunBook/FunBook.WebForms/FunAreaPages/FunMasterPage.master.cs
+++ FunBook/FunBook.WebForms/FunAreaPages/FunMasterPage.master.cs
@@ -5,13 +5,18 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using FunBook.Data;
+
 namespace FunBook.WebForms.FunAreaPages
 {
-    public partial class FunMasterPage : System.Web.UI.MasterPage
+    public partial class FunMasterPage : MasterPage
     {
+        FunBookDbContext db = new FunBookDbContext();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            categories.DataSource = db.Categories.ToList();
+            categories.DataBind();
         }
     }
 }
