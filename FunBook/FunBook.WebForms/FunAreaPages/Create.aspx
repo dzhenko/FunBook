e0f7c5eb0d71b9f3bc0fc04de0aca7a54137b9f5
Viktor90 <viktor102690@gmail.com>
2014-10-21 23:32:36 +0300

    All user's jokes/links/pictures displayed in tabs->listViews. If there aren't any, the tabs are disabled. No css added. Remains to add paging and make jokes clickable.

--- FunBook/FunBook.WebForms/FunAreaPages/Create.aspx
+++
@@ -1,4 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FunAreaPages/FunMasterPage.master" AutoEventWireup="true" CodeBehind="Create.aspx.cs" Inherits="FunBook.WebForms.FunAreaPages.Create" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolderFunArea" runat="server">
-    <h1>Create/Update</h1>
-</asp:Content>
---
+++ FunBook/FunBook.WebForms/FunAreaPages/OwnFun.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using FunBook.Data;
+using Microsoft.AspNet.Identity;
+
+namespace FunBook.WebForms.FunAreaPages
+{
+    public partial class Create : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var data = FunBookData.Create();
+            var id = Context.User.Identity.GetUserId();
+            var jokes = data.Jokes.All().Where(uid => uid.UserId == id).ToList();
+            var links = data.Links.All().Where(uid => uid.UserId == id).ToList();
+            var pics = data.Pictures.All().Where(uid => uid.UserId == id).ToList();
+
+            if (jokes.Count == 0)
+            {
+                this.liJoke.Attributes.Add("class", "disabled");
+                this.liPic.InnerHtml = "<a>Pics</a>"; // I know it's not very pretty, but didn't found other way
+            }
+
+            if (links.Count == 0)
+            {
+                this.liLink.Attributes.Add("class", "disabled");
+                this.liLink.InnerHtml = "<a>Links</a>";
+            }
+
+            if (pics.Count == 0)
+            {
+                this.liPic.Attributes.Add("class", "disabled");
+                this.liPic.InnerHtml = "<a>Pics</a>";
+            }
+
+            this.ListViewJokes.DataSource = jokes;
+            this.ListViewLinks.DataSource = links;
+            this.ListViewPics.DataSource = pics;
+
+            this.DataBind();
+        }
+    }
+}
