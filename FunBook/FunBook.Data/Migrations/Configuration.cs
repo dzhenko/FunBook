cdcde803672898b45ec986912cf150b3f2e70430
fast4y <dzhenko@gmail.com>
2014-10-22 12:13:16 +0300

    Added 'Crawer' to steal and fill our database with sample jokes. Crawer will only work on empty DB(having no categories) and having at least 1 user. 153 jokes added with 27 categories and 1610 tags (5439 joke-tag connections).

--- FunBook/FunBook.Data/Migrations/Configuration.cs
+++ FunBook/FunBook.Data/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace FunBook.Data.Migrations
 {
+    using FunBook.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -13,17 +14,84 @@ namespace FunBook.Data.Migrations
             this.AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(FunBook.Data.FunBookDbContext context)
+        protected override void Seed(FunBookDbContext context)
         {
             if (!context.Categories.Any())
             {
-                context.Categories.Add(new Models.Category() { Name = "Blonds" });
-                context.Categories.Add(new Models.Category() { Name = "Big Brother" });
-                context.Categories.Add(new Models.Category() { Name = "Microsoft" });
-                context.Categories.Add(new Models.Category() { Name = "Animals" });
-                context.Categories.Add(new Models.Category() { Name = "Dirty" });
-                context.Categories.Add(new Models.Category() { Name = "Chuck Noris" });
+                this.GenerateRandomContent(context);
             }
+        }
+
+        private void GenerateRandomContent(FunBookDbContext context)
+        {
+            if (!context.Users.Any())
+            {
+                throw new ArgumentException("Register at least one user first !");
+            }
+
+            var userId = context.Users.FirstOrDefault().Id;
+
+            System.Net.WebClient client = new System.Net.WebClient();
+            var endpoints = new string[] {"popular-jokes","latest-jokes", "joke-of-the-day", "animal-jokes", "blonde-jokes",
+            "boycott-these-jokes", "clean-jokes", "family-jokes","holiday-jokes", "how-to-be-insulting", "insult-jokes" ,"miscellaneous-jokes",
+            "national-jokes" ,"office-jokes" ,"political-jokes","pop-culture-jokes","racist-jokes","relationship-jokes",
+            "religious-jokes","school-jokes","science-jokes","sex-jokes","sexist-jokes","sports-jokes","technology-jokes","word-play-jokes","yo-momma-jokes"};
+
+            var tagDict = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.List<Joke>>();
+
+            for (int j = 0; j < endpoints.Length; j++)
+            {
+                var jokeCategory = endpoints[j].Replace("-jokes", "");
+                string downloadString = client.DownloadString(string.Format("http://www.laughfactory.com/jokes/{0}", endpoints[j]));
+                var jokes = System.Text.RegularExpressions.Regex.Split(downloadString, @"<p id=""joke_\d+"">");
+                
+                context.Categories.Add(new Category() { Name = jokeCategory });
+                context.SaveChanges();
+
+                var categoryId = context.Categories.FirstOrDefault(c => c.Name == jokeCategory).Id;
+
+                for (int i = 1; i < jokes.Length; i++)
+                {
+                    var joke = jokes[i].TrimStart();
+                    joke = joke.Substring(0, joke.IndexOf("      "));
+
+                    var tags = System.Text.RegularExpressions.Regex.Split(joke, @"\W+");
+                    
+                    var jokeObj = new Joke()
+                    {
+                        CategoryId = categoryId,
+                        UserId = userId,
+                        IsAnonymous = true,
+                        Title = joke.Substring(0, 10),
+                        Text = joke,
+                    };
+
+                    foreach (var tag in tags)
+                    {
+                        if (!tagDict.ContainsKey(tag.ToLower()))
+                        {
+                            tagDict[tag.ToLower()] = new System.Collections.Generic.List<Joke>();
+                        }
+
+                        tagDict[tag.ToLower()].Add(jokeObj);
+                    }
+
+                    context.Jokes.Add(jokeObj);
+                }
+
+                context.SaveChanges();
+            }
+
+            foreach (var kvPair in tagDict)
+            {
+                context.Tags.Add(new Tag() 
+                {
+                    Name = kvPair.Key,
+                    Jokes = kvPair.Value
+                });
+            }
+
+            context.SaveChanges();
         }
     }
 }
