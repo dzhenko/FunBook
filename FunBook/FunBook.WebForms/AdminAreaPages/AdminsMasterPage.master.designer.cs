b0caedfffbdcce45284ef39e3841ec41e5ef9a5a
TCVD <viktordakov97@gmail.com>
2014-10-22 22:12:27 +0300

    Added sitemaps to the public and admin parts.

--- FunBook/FunBook.WebForms/AdminAreaPages/AdminsMasterPage.master.designer.cs
+++ FunBook/FunBook.WebForms/AdminAreaPages/AdminsMasterPage.master.designer.cs
@@ -13,6 +13,15 @@ namespace FunBook.WebForms.AdminAreaPages {
     public partial class AdminsMasterPage {
         
         /// <summary>
+        /// SiteMap1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMap1;
+        
+        /// <summary>
         /// ContentPlaceHolderAdminArea control.
         /// </summary>
         /// <remarks>
--- FunBook/FunBook.WebForms/FunAreaPages/FunMasterPage.master.designer.cs
+++ FunBook/FunBook.WebForms/FunAreaPages/FunMasterPage.master.designer.cs
@@ -22,6 +22,15 @@ namespace FunBook.WebForms.FunAreaPages {
         protected global::System.Web.UI.WebControls.Repeater categories;
         
         /// <summary>
+        /// SiteMap1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMap1;
+        
+        /// <summary>
         /// ContentPlaceHolderFunArea control.
         /// </summary>
         /// <remarks>
