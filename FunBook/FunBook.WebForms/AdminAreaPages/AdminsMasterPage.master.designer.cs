7b0e25d1f5f209f631d0a7dc200412fd24600123
TCVD <viktordakov97@gmail.com>
2014-10-22 22:14:33 +0300

    Fixed layout bugs for the home page.

--- FunBook/FunBook.WebForms/AdminAreaPages/AdminsMasterPage.master.designer.cs
+++ FunBook/FunBook.WebForms/AdminAreaPages/AdminsMasterPage.master.designer.cs
@@ -13,13 +13,13 @@ namespace FunBook.WebForms.AdminAreaPages {
     public partial class AdminsMasterPage {
         
         /// <summary>
-        /// SiteMap1 control.
+        /// SiteMapPath1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.SiteMapPath SiteMap1;
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMapPath1;
         
         /// <summary>
         /// ContentPlaceHolderAdminArea control.
